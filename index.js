6c1eeeb663646610e7660e0ad2b6921ce581ddc2
Michi <bogensbergermichael@gmail.com>
2022-02-01 17:32:34 +0100

    changes

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express')
+var mysql = require('mysql');
+const bodyparser = require('body-parser');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var conn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database: 'gymtracker'
+});
+
+conn.connect((err)=>{
+  if(!err)
+  console.log("DB connection -> success");
+  else
+  console.log("DB connection -> failed -> Error:" + JSON.stringify(err));
+});
+
+app.use(bodyparser.json());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+
+
+app.get('/', (req, res) => {
+  conn.query('SELECT * FROM user', (err, rows, fields) => {
+
+    if(!err) {
+      console.log(rows);
+    }
+    else {
+      console.log(err);
+    }
+
+  })
+  res.send('Hello World!')
 })
